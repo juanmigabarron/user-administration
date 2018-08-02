c70101855ecffc95f46c3c4e4c84e3abcbd33f7c
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-02 22:38:12 +0200

    New models User and BankAccount

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    created_by = models.ForeignKey('administration.User', on_delete=models.CASCADE, blank=True, null=True)
+
+
+class BankAccount(models.Model):
+    iban = models.CharField(max_length=34)
+    user = models.ForeignKey('administration.User', on_delete=models.CASCADE)
