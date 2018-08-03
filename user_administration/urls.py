27d6148b2ad7cb8fb4f1a134f7c25526a286e775
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-03 14:27:25 +0200

    Configure admin and auth urls

--- user_administration/urls.py
+++ user_administration/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.login_template = 'administration/login.html'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('auth/', include('social_django.urls', namespace='social'))
 ]
