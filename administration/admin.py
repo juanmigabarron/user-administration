ca3bba7cf6cc615b6cf7495ded2453397fba4b5e
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-04 10:09:16 +0200

    Register Models with a custom ModelAdmin

--- administration/admin.py
+++ administration/admin.py
@@ -1,7 +1,34 @@
+from django.contrib import admin
+
 from user_administration.admin import admin_site
-from django.contrib.auth.admin import UserAdmin
 
-from .models import User, BankAccount
+from .models import BankAccount, User
 
-admin_site.register(User, UserAdmin)
-admin_site.register(BankAccount)
+
+@admin.register(User, site=admin_site)
+class UserModelAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        permission = False
+        if obj and request.user == obj.created_by:
+            permission = True
+        return permission
+    
+    def has_delete_permission(self, request, obj=None):
+        permission = False
+        if obj and request.user == obj.created_by:
+            permission = True
+        return permission
+
+@admin.register(BankAccount, site=admin_site)
+class BankAccountModelAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        permission = False
+        if obj and request.user == obj.user.created_by:
+            permission = True
+        return permission
+    
+    def has_delete_permission(self, request, obj=None):
+        permission = False
+        if obj and request.user == obj.user.created_by:
+            permission = True
+        return permission
