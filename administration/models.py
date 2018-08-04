0126d028ea60d9fd55f1491f782b173f04ca8601
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-04 20:54:57 +0200

    Use iban validator on model field

--- administration/models.py
+++ administration/models.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.db import models
 
+from .validators import validate_iban
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    iban = models.CharField(max_length=34)
+    iban = models.CharField(max_length=34, validators=[validate_iban])
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
