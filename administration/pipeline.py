f76a87e1073aa24b7ec3b95f4de0719ef8adca64
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-05 00:05:06 +0200

    Clean code

--- administration/pipeline.py
+++ administration/pipeline.py
@@ -1,6 +1,5 @@
 def add_superpowers(backend, user, response, *args, **kwargs):
     ''' Converts user into superuser '''
-
     if user and not user.is_superuser:
         user.is_superuser = True
         user.save()
