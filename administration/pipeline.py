06e1e17e45c30f8d08e92c5de5266dc1107ada3e
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-04 10:07:51 +0200

    Social Auth pipelines and add custom pipeline

---
+++ administration/pipeline.py
@@ -0,0 +1,6 @@
+def add_superpowers(backend, user, response, *args, **kwargs):
+    ''' Converts user into superuser '''
+
+    if user and not user.is_superuser:
+        user.is_superuser = True
+        user.save()
--- user_administration/settings/base.py
+++ user_administration/settings/base.py
@@ -135,6 +135,19 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = get_from_env(
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = get_from_env(
     'GOOGLE_OAUTH_CLIENT_SECRECT')
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'administration.pipeline.add_superpowers',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
