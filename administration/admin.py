01492c273b761dfcaa812c4251d311a6399932b1
Juan Miguel Gabarron <juanmigabarron@gmail.com>
2018-08-04 10:18:28 +0200

    Remove BankAccount Model and list_display on User model Admin

--- administration/admin.py
+++ administration/admin.py
@@ -2,33 +2,21 @@ from django.contrib import admin
 
 from user_administration.admin import admin_site
 
-from .models import BankAccount, User
+from .models import User
 
 
 @admin.register(User, site=admin_site)
 class UserModelAdmin(admin.ModelAdmin):
-    def has_change_permission(self, request, obj=None):
-        permission = False
-        if obj and request.user == obj.created_by:
-            permission = True
-        return permission
-    
-    def has_delete_permission(self, request, obj=None):
-        permission = False
-        if obj and request.user == obj.created_by:
-            permission = True
-        return permission
+    list_display = ('first_name', 'last_name', 'iban')
 
-@admin.register(BankAccount, site=admin_site)
-class BankAccountModelAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         permission = False
-        if obj and request.user == obj.user.created_by:
+        if obj and request.user == obj.created_by:
             permission = True
         return permission
     
     def has_delete_permission(self, request, obj=None):
         permission = False
-        if obj and request.user == obj.user.created_by:
+        if obj and request.user == obj.created_by:
             permission = True
         return permission
--- administration/models.py
+++ administration/models.py
@@ -5,9 +5,5 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-
-class BankAccount(models.Model):
     iban = models.CharField(max_length=34)
-    user = models.ForeignKey('administration.User', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
